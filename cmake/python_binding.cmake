2129ff3738b4880fd6ef36f9b913fd9619a475f4
franck.ledoux <franck.ledoux@cea.fr>
2023-01-07 14:33:09 +0100

    - cmake issue with Python 3, try to remove link to Python2

--- cmake/python_binding.cmake
+++ cmake/python_binding.cmake
@@ -5,13 +5,13 @@ find_package (SWIG 3 REQUIRED)
 # Par défaut on utilise Python 2
 
 #find_package (Python REQUIRED COMPONENTS Interpreter Development)	# Rem : Python3 a la priorité => inutilisé car empêche l'accès à Python2
-find_package (Python3 REQUIRED COMPONENTS Interpreter Development)
-set (PYTHON_BINDING_DIR ${CMAKE_INSTALL_LIBDIR}/python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages/)
-set (Python_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
-set (Python_EXECUTABLE ${Python3_EXECUTABLE})
-set (Python_VERSION ${Python3_VERSION})
-set (Python_LIBRARIES ${Python3_LIBRARIES})
-set (Python_LIBRARY_DIRS ${Python3_LIBRARY_DIRS})
+	find_package (Python3 REQUIRED COMPONENTS Interpreter Development)
+	set (PYTHON_BINDING_DIR ${CMAKE_INSTALL_LIBDIR}/python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}/site-packages/)
+	set (Python_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
+	set (Python_EXECUTABLE ${Python3_EXECUTABLE})
+	set (Python_VERSION ${Python3_VERSION})
+	set (Python_LIBRARIES ${Python3_LIBRARIES})
+	set (Python_LIBRARY_DIRS ${Python3_LIBRARY_DIRS})
 include (${SWIG_USE_FILE})
 
 
