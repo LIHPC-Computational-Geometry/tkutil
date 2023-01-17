bf7b7bb2e7f4d2b7015ae2e6188f17fa003db06f
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2023-01-17 14:04:48 +0100

    Bug fix in test_link main

--- test_link/main.cpp
+++ test_link/main.cpp
@@ -5,7 +5,7 @@
 #include <TkUtil/Exception.h>
 
 int main() {
-    const std::string string_version("VInfinite");
+    const std::string string_version("2.3.5");
     TkUtil::Version tkutil_version(string_version);
     std::cout << "Version " << tkutil_version.getMajor() << "." << tkutil_version.getMinor() << std::endl;
     return 0;
