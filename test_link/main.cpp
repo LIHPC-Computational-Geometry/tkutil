9f14b0cff708f53ac1f82b94ef92a01186f918af
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2023-01-17 13:34:12 +0100

    New test_link main

--- test_link/main.cpp
+++ test_link/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
+#include <TkUtil/Version.h>
 #include <TkUtil/util_config.h>
 #include <TkUtil/Exception.h>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const string string_version("VInfinite");
+    TkUtil::Version tkutil_version(string_version);
+    std::cout << "Version " << tkutil_version.getMajor() << "." << tkutil_version.getMinor() << std::endl;
     return 0;
 }
